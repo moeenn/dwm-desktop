010627fe7939244a1f33d1603c0e6cfa411016e7
moeenn <moeen.v8@gmail.com>
2023-03-24 15:04:19 +0500

    removed rects

--- dwm-6.4/dwm.c
+++ dwm-6.4/dwm.c
@@ -748,7 +748,7 @@ drawbar(Monitor *m)
 				// drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
-			// drw_rect(drw, x, 0, w, bh, 1, 1);
+			drw_rect(drw, x, 0, w, bh, 1, 1);
 		}
 	}
 	drw_map(drw, m->barwin, 0, 0, m->ww, bh);
