8bf91e3b828443fd6f712b5cddc4e40f40c8954b
moeenn <moeen.v8@gmail.com>
2024-03-30 23:38:27 +0500

    updated configs

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 0;            /* 0 means bottom bar */
 static const int barpadding = 23;
-static const char *fonts[] = {"JetBrains Mono:size=9"};
+static const char *fonts[] = {"Ubuntu Mono:size=11"};
 static const char fg_dull[] = "#969279";
 static const char bg_norm[] = "#32332B";
 static const char fg_norm[] = "#F2F0DA";
