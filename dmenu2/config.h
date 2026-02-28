710eb62b89b4007267f091a534f547dfd7067af6
moeenn <moeen.v8@gmail.com>
2026-02-28 14:12:54 -0500

    updated dwm colors

--- dmenu2/config.h
+++ dmenu2/config.h
@@ -6,9 +6,9 @@ static const char* fonts[] = { "Roboto Medium:size=9" };
 static const char* prompt = NULL;
 static const char* colors[SchemeLast][2] = {
     /* fg       bg       */
-    [SchemeNorm] = { "#d8d8d8", "#181818" },
-    [SchemeSel] = { "#d8d8d8", "#777777" },
-    [SchemeOut] = { "#d8d8d8", "#777777" }, // border.
+    [SchemeNorm] = { "#2e3436", "#ffffff" },
+    [SchemeSel] = { "#f9fcfe", "#3584e4" },
+    [SchemeOut] = { "#d7d7d7", "#d7d7d7" }, // border.
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 7;
